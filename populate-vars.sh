f2909077fbe2b9cd47e0f24b790f1f07f1321f00
root <root@bastion.65de.example.opentlc.com>
2018-08-06 04:07:49 +0000

    hello

--- populate-vars.sh
+++ populate-vars.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+GUID=`hostname | cut -d"." -f2`
+host_path="lab/hosts"
 nodes=( $(ansible all --list-hosts) ) 
-#echo ${#nodes[@]}
 processed=${nodes[@]:2}
+
+echo "[bastion]" > $host_path
+echo  "bastion.$GUID.example.opentlc.com" >> $host_path
+
+
+
+
 for i in ${processed[@]}
 do
   echo "nodes : ${i}"
