3675c3391147478a02a3fa71df44c08f956a144f
root <root@bastion.f3d8.example.opentlc.com>
2018-09-23 10:38:09 +0000

    smoke test fixes

--- provision-cluster.sh
+++ provision-cluster.sh
@@ -7,10 +7,14 @@ backup_path="lab/backup"
 host_path="$backup_path/hosts_generated"
 ansible_host_path="/etc/ansible/hosts"
 vars_path="lab/host_vars/localhost.yml"
+smoke_test_param_path="lab/ocp_template/smoketest/param/smoke-param"
+
+
 
 mkdir -p $backup_path
 ## replce the GUID
 sed -i "s/sed_guid:.*/sed_guid: $GUID/g" $vars_path
+sed -i "s/APPLICATION_DOMAIN=.*/APPLICATION_DOMAIN=nodejs-mongo-persistent-smoke-test.apps.$GUID.example.opentlc.com/g" $smoke_test_param_path
 
 sudo ansible-playbook -v -f 20 "lab/install_ocp.yml"
 
